6f291ebade44b0dc2fc3085d4810418392559211
Jason Humbert <jasonhumbert@outlook.com>
2021-12-10 12:03:41 +0000

    Add test to check if DockingStation releases a Bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,9 @@
 require 'docking_station'
+require 'bike'
 
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
+
+  it { expect(subject.release_bike).to be_a Bike }
+  
 end
