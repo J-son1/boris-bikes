5807dbc8f618a0ea472680f823f7c7635263a9ae
Jason Humbert <jasonhumbert@outlook.com>
2021-12-11 23:42:36 +0000

    Modify dock test/pass tests: Make DockingStation have a default capacity of 20 bikes

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -26,22 +26,25 @@ describe DockingStation do
     it { is_expected.to respond_to(:dock).with(1).argument }
 
     it 'raises an error when full' do
-      subject.dock(Bike.new)
+      20.times { subject.dock Bike.new }
       expect { subject.dock Bike.new }.to raise_error 'Docking station full'
     end
 
     it 'docks a bike' do
       bike = Bike.new
-      expect(subject.dock(bike)).to eq bike
+      expect(subject.dock(bike)).to eq [bike]
     end
   
+  end
+  
+  describe '#bikes' do
+    it { is_expected.to respond_to :bikes }
+    
     it 'returns docked bikes' do
-      bike = Bike.new
-      subject.dock(bike)
-      expect(subject.bike).to eq bike
+      bikes = []
+      20.times { bikes << Bike.new }
+      bikes.each { |bike| subject.dock bike }
+      expect(subject.bikes).to eq bikes
     end
   end
-
-  it { is_expected.to respond_to :bike }
-
 end
