bfe2ff881a302a0dd4b34cc19a22b422bf6c7609
Jason Humbert <jasonhumbert@outlook.com>
2021-12-24 17:07:54 +0000

    Add test doubles and amend affected tests

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,8 +4,8 @@ require 'bike'
 describe DockingStation do
   subject(:station) { DockingStation.new }
 
-  let(:bike) { Bike.new }
-  let(:dock_bike) { station.dock(bike) }
+  let(:bike) { double(:bike) }
+  let(:dock_bike) { station.dock(bike) }    # Bike class
 
   it 'has a default capacity' do
     expect(station.capacity).to eq DockingStation::DEFAULT_CAPACITY
@@ -14,15 +14,15 @@ describe DockingStation do
   describe 'initialization' do
     it 'defaults capacity' do
       described_class::DEFAULT_CAPACITY.times do
-        station.dock(bike)
+        station.dock(bike)    # Bike class
       end
-      expect{ station.dock(bike) }.to raise_error 'Docking station full'
+      expect{ station.dock(bike) }.to raise_error 'Docking station full'    # Bike class
     end
     
     it 'has a variable capacity' do
       docking_station = DockingStation.new(50)
-      50.times { docking_station.dock Bike.new }
-      expect{ docking_station.dock Bike.new }.to raise_error 'Docking station full'
+      50.times { docking_station.dock bike }    # Bike class
+      expect{ docking_station.dock bike }.to raise_error 'Docking station full'   # Bike class
     end
   end
 
@@ -35,6 +35,8 @@ describe DockingStation do
       end
 
       it 'does not release broken bikes' do
+        allow(bike).to receive(:report_broken)
+        allow(bike).to receive(:broken?).and_return(true)
         bike.report_broken
         dock_bike
         expect { station.release_bike }.to raise_error 'Bike not available'
@@ -45,12 +47,15 @@ describe DockingStation do
       before { dock_bike }
 
       it 'releases a bike' do
-        expect(station.release_bike).to eq bike
+        allow(bike).to receive(:broken?).and_return(false)
+        expect(station.release_bike).to eq bike   # Bike class
       end
 
-      it 'releases a bike that is working' do
+      it 'releases a working bike' do
+        allow(bike).to receive(:broken?).and_return(false)
+        allow(bike).to receive(:working?).and_return(true)
         bike = station.release_bike
-        expect(bike).to be_working
+        expect(bike).to be_working    # Bike class
       end
     end
   end 
@@ -60,19 +65,20 @@ describe DockingStation do
 
     context 'when full' do
       it 'raises an error' do
-        station.capacity.times { station.dock Bike.new }
-        expect { station.dock Bike.new }.to raise_error 'Docking station full'
+        station.capacity.times { station.dock Bike.new }    # Bike class
+        expect { station.dock Bike.new }.to raise_error 'Docking station full'    # Bike class
       end
     end
 
     context 'when not full' do
       it 'docks a working bike' do
-        expect(station.dock(bike)).to eq [bike]
+        expect(station.dock(bike)).to eq [bike]   # Bike class
       end
 
       it 'docks a broken bike' do
+        allow(bike).to receive(:report_broken)
         bike.report_broken
-        expect(station.dock(bike)).to eq [bike]
+        expect(station.dock(bike)).to eq [bike]   # Bike class
       end
     end
   end
